6125c373645edb3433f04f5c0cbc801b9190b7b9
Mariyam Mayan <bluefoood@gmail.com>
2026-08-07 13:22:19 +0800

    Add project files.

---
+++ DDAC/Controllers/JobSeeker/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using DDAC.Models.JobSeeker;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InclusiveEmployment.Controllers
+{
+    public class JobSeekerController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ DDAC/Models/JobSeeker/Login.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DDAC.Models.JobSeeker
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
